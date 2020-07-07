09f15756ca367343175c69710d985fe5e9574d6f
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-07-06 23:11:32 -0300

    Get single character route

--- controllers/character.js
+++ controllers/character.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('../middlewares/asyncHandler');
+const ErrorResponse = require('../utils/errorResponse');
 const Character = require('../models/Character');
 
 exports.getCharacters = asyncHandler(async (req, res, next) => {
@@ -7,4 +8,14 @@ exports.getCharacters = asyncHandler(async (req, res, next) => {
   return res
     .status(200)
     .json({ success: true, count: characters.length, data: characters });
+});
+
+exports.getCharacter = asyncHandler(async (req, res, next) => {
+  const character = await Character.findOne({ char_id: req.params.id });
+
+  if (!character) {
+    return next(new ErrorResponse('Character not found', 404));
+  }
+
+  return res.status(200).json({ success: true, data: character });
 });
--- routes/characters.js
+++ routes/characters.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getCharacters } = require('../controllers/character');
+const { getCharacters, getCharacter } = require('../controllers/character');
 
 const router = express.Router();
 
 router.route('/').get(getCharacters);
+router.route('/:id').get(getCharacter);
 
 module.exports = router;
